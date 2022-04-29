d8c7ed5a695ab1a3704ec52dd98cf6bbf19754d0
monacoglynn <96494895+monacoglynn@users.noreply.github.com>
2022-04-29 16:58:46 -0700

    added a redundency check to site to prevent cities from being listed twice. also added more weather data and stylized the current days wether.

--- assets/js/stript.js
+++ assets/js/stript.js
@@ -5,6 +5,20 @@ var formSubmit = document.querySelector('.form-submit');
 var cityList = document.querySelector('.searched-cities');
 var listOfCities = []
 
+var todaysWeatherArray = ['.name', '.weather.icon', '`${.main.temp} F`', '.main.feels_like', '.wind.speed', '.main.humidity', '.main.temp_min', '.main.temp_max', ]
+/* array of data:
+main page
+city name, date, icon => (weather.icon)???
+temp, feels_like wind speed(wind[speed]), humidity, uv index, temp_max, temp_min,
+5day forecast
+date, icon
+temp, wind, humidity    
+ */
+
+if (localStorage.getItem('savedCity')) {
+    listOfCities = JSON.parse(localStorage.getItem('savedCity'))
+}
+
 function init() {
     let storedCities = JSON.parse(localStorage.getItem('savedCity'))
     if (storedCities == null) return;
@@ -30,25 +44,42 @@ function fetchWeather(location) {
             console.log(data);
 
             let city = location.replace(' ', '-');
-            listOfCities.push(city);
-            console.log(city);
-            //save data to localStorage under variable name of the city.
-            localStorage.setItem('savedCity', JSON.stringify(listOfCities));
+
             cityInput.value = '';
 
             function displayWeather(event) {
                 weatherToday.innerHTML = ''; // this clears whatever city info was in the main div
+                weatherToday.setAttribute('style', 'border: 2px solid #000000;')
                 // this grabs the data from localStorage to display on the page
                 let cityWeatherData = JSON.parse(localStorage.getItem(city))
                 let cityName = document.createElement('h2');
+                var iconURL = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+                var iconEl = document.createElement('img')
+                iconEl.setAttribute('src', iconURL);
                 // this grabs the cities name
-                cityName.textContent = data.name;
+                cityName.textContent = `${data.name}`;
                 weatherToday.appendChild(cityName);
+                weatherToday.appendChild(iconEl);
                 let currentTemp = document.createElement('h3');
                 // this grabs the current temperature 
-                currentTemp.textContent = `${data.main.temp} F`;
+                currentTemp.textContent = `Current Temp: ${data.main.temp} F`;
                 weatherToday.appendChild(currentTemp);
+                let windSpeed = document.createElement('h3');
+                windSpeed.textContent = `Wind Speed: ${data.wind.speed} MPH`
+                weatherToday.appendChild(windSpeed);
+                let humid = document.createElement('h3');
+                humid.textContent = `Humidity: ${data.main.humidity} %`;
+                weatherToday.appendChild(humid);
+
+
                 // this creates the city button on the list
+                console.log(city)
+                if (!redundancyCheck(city)) {
+                    console.log('the limit does not exist (actually this city is in the array already)')
+                    return
+                }
+                saveToLocal(city)
+
                 let cityButton = document.createElement('button')
                 cityButton.textContent = data.name;
                 cityButton.setAttribute('class', city);
@@ -58,11 +89,29 @@ function fetchWeather(location) {
         })
 }
 
+function redundancyCheck(city) {
+    for (let i = 0; i < listOfCities.length; i++) {
+        if (listOfCities[i].toLowerCase() === city.toLowerCase()) {
+            return false
+        }
+    }
+
+    return true
+}
+
+
+
+function saveToLocal(city) {
+    listOfCities.push(city);
+    console.log(city);
+    //save data to localStorage under variable name of the city.
+    localStorage.setItem('savedCity', JSON.stringify(listOfCities));
+}
+
 
 formSubmit.addEventListener('click', function (event) {
     event.preventDefault();
     fetchWeather(cityInput.value.trim());
-
 })
 
 cityList.addEventListener('click', function (event) {
